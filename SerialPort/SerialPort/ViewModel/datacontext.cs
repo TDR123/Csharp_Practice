accb3ef356fa149bfc1b8d5434c44bef59966621
tangwei <17778577621@163,com>
2025-03-16 12:20:49 +0800

    串口连接demo

---
+++ SerialPort/SerialPort/ViewModel/datacontext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SerialPort.ViewModel
+{
+    internal partial class datacontext : ObservableObject
+    {
+
+        // 使用ObservableProperty特性声明可观察属性
+        [ObservableProperty]
+        private string _name;
+
+        [ObservableProperty]
+        private int _age;
+
+    }
+}
