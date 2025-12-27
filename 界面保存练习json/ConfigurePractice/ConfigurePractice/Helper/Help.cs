f1e2847c51f5521f17a5e9787ac8a0843bfabf4f
tangwei <17778577621@163,com>
2025-12-27 23:23:58 +0800

    增加了一个新项目，里面练习了handycontrol和materialdesign

---
+++ 界面保存练习json/ConfigurePractice/ConfigurePractice/Helper/Help.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ConfigurePractice.Helper
+{
+ 
+        // 布尔值转字符串转换器
+        public class BoolToTextConverter : IValueConverter
+        {
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value is bool boolValue)
+                {
+                    // true 显示 "1"，false 显示 "0"
+                    return boolValue ? "已连接" : "连接";
+                }
+                return "连接";
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                // 如果需要双向绑定，实现此方法
+                throw new NotImplementedException();
+            }
+        }
+    
+}
---
+++ 界面保存练习json/ConfigurePractice/ConfigurePractice/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ConfigurePractice.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+namespace ConfigurePractice.ViewModel
+{
+   public partial class MainWindowViewModel:ObservableObject
+    {
+        [ObservableProperty] MainWindowModel mainWindowModel;
+        //[ObservableProperty] string ip="192.168.1.111";
+        [ObservableProperty] bool isConnected = false;
+        [ObservableProperty] bool flag = false;
+
+        //时间
+       [ObservableProperty] string currentTime = DateTime.Now.ToString("HH:mm:ss");
+        public MainWindowViewModel()
+        {
+            mainWindowModel = new MainWindowModel();
+            MainWindowModel.IpAddress = "192.168.0.1";
+
+            //
+            // 每秒刷新一次
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += (_, __) => CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+            timer.Start();
+        }
+
+        //连接按钮命令
+        [RelayCommand]
+        private async Task ipConnection()
+        {
+            // 模拟异步连接操作
+             Flag= true;
+            await Task.Delay(3000);
+            IsConnected= !IsConnected;
+            Flag = false;
+        }
+    }
+}
