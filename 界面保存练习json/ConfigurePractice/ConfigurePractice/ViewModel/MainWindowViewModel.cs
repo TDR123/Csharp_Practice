e8a7cb990907b04f9ce7cfbd570a178a6c2d7111
tangwei <17778577621@163,com>
2025-12-28 00:22:48 +0800

    实现了进度条百分比，异步执行

--- 界面保存练习json/ConfigurePractice/ConfigurePractice/ViewModel/MainWindowViewModel.cs
+++ 界面保存练习json/ConfigurePractice/ConfigurePractice/ViewModel/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ConfigurePractice.Model;
+using ConfigurePractice.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Threading;
 namespace ConfigurePractice.ViewModel
 {
@@ -15,14 +18,14 @@ namespace ConfigurePractice.ViewModel
         //[ObservableProperty] string ip="192.168.1.111";
         [ObservableProperty] bool isConnected = false;
         [ObservableProperty] bool flag = false;
-
+        [ObservableProperty] UserControl content;
         //时间
-       [ObservableProperty] string currentTime = DateTime.Now.ToString("HH:mm:ss");
+        [ObservableProperty] string currentTime = DateTime.Now.ToString("HH:mm:ss");
         public MainWindowViewModel()
         {
             mainWindowModel = new MainWindowModel();
             MainWindowModel.IpAddress = "192.168.0.1";
-
+            Content= new UserControl();
             //
             // 每秒刷新一次
             var timer = new DispatcherTimer
@@ -42,6 +45,25 @@ namespace ConfigurePractice.ViewModel
             await Task.Delay(3000);
             IsConnected= !IsConnected;
             Flag = false;
+        }
+
+        //
+        [RelayCommand]
+        private void showPage1()
+        {
+            Page1ViewModel p1vm = new Page1ViewModel();
+            Content = new page1(p1vm);
+            Task.Run(async () =>
+            {
+                for (int i = 0; i < 101; i++)
+                {
+                   Dispatcher.CurrentDispatcher.Invoke(() => p1vm.ProgressValue = i);
+                    await Task.Delay(100);
+                }
+            });
+           
+        
+
         }
     }
 }
---
+++ 界面保存练习json/ConfigurePractice/ConfigurePractice/ViewModel/Page1ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ConfigurePractice.ViewModel
+{
+    public partial class Page1ViewModel:ObservableObject
+    {
+        [ObservableProperty] double progressValue = 0;
+        public Page1ViewModel()
+        {
+
+        }
+
+    }
+}
